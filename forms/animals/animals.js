98062da0a689e50bfb575a1ae7c543bd212be1a7
alyssahuey <79379576+alyssahuey@users.noreply.github.com>
2021-03-15 16:54:31 -0500

    done

---
+++ forms/animals/animals.js
@@ -0,0 +1,13 @@
+/*
+let myAnimals = ["dog", "cat", "horse", "meerkat"]
+
+for (i = 0; i < myAnimals.length; i++) {
+    console.log(`${myAnimals[i]}`)
+}
+
+let addAnimals = prompt("Name an animal.")
+myAnimals.push(addAnimals.toLowerCase())
+
+console.log(`The last animal is a/an ${myAnimals[i]}.`)
+*/
+//only output the last animal
---
+++ forms/calculate/calculate.js
@@ -0,0 +1,13 @@
+/*
+let number1 = Number(prompt("Enter the first number."))
+let number2 = Number(prompt("Enter the second number."))
+let number3 = Number(prompt("Enter the third number."))
+
+function calcAvgSquare(num1, num2, num3){
+    let answer = ((num1 + num2 + num3)/3)*Math.pow(num1,2)
+    return answer
+}
+
+let finalAnswer = calcAvgSquare(number1, number2, number3)
+console.log(`The answer is ${finalAnswer}.`)
+*/
